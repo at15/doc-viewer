345ecd09842c7f4de44e73a63bd11f0c8c78ca9d
at15 <at15@outlook.com>
2015-06-19 23:10:15 +0800

    Move server code to lib/server

---
+++ bin/doc-viewer.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Pillar on 2015/6/19.
+ */
+
+var server = require('./lib/server');
+// TODO:set it to current working dir.
+//server.setDocRoot();
+server.start();
--- index.js
+++ index.js
@@ -3,34 +3,6 @@
  */
 
 'use strict';
-// TODO: config
-var docRoot = './doc';
-var port = 3000;
 
-console.log(docRoot);
-
-var express = require('express');
-var app = express();
-var path = require('path');
-var fs = require('fs');
-var render = require('./lib/render');
-
-app.use(express.static('public'));
-app.get('/doc', function (req, res) {
-    var fileName = path.join(docRoot, req.query.f);
-    console.log('get request for file ', fileName);
-    // TODO:support folder
-    //var readeMe = path.dirname(fileName) + '/README.md';
-    if (!fs.existsSync(fileName)) {
-        res.status(404).send('file ' + fileName + 'doesn\'t exist');
-    } else {
-        res.status(200).send(
-            //render.highlight(fs.readFileSync(fileName, {encoding: 'UTF-8'}))
-            '<html><head><title>demo</title><link rel="stylesheet" href="default.css"/></head><body>' +
-            render.useToc(fs.readFileSync(fileName, {encoding: 'UTF-8'})) + '</body></html>'
-        );
-    }
-});
-
-app.listen(port);
-console.log('server init!');
+var server = require('./lib/server');
+server.start();
