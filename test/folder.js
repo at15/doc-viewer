15a78166feb8be1fe3f785505a689258598cf6ac
at15 <at15@outlook.com>
2015-07-13 01:11:23 +0800

    Add folder support
    - remove the index.html in public
    - add route for '/' to show the folder in doc folder
    
    now need to add the links and support readme and folders at same time

---
+++ test/folder.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Pillar on 2015/7/12.
+ */
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+var folder = require('../lib/util/folder');
+
+describe('Traverse folder', function () {
+    it('depth first', function () {
+        // TODO:refactor, use filter to add the folder prefix
+        var dfsResult = folder.dfs('./test/traverse-me');
+        expect(dfsResult).to.eql(
+            [
+                './test/traverse-me/a/a.md',
+                './test/traverse-me/c.md'
+            ]
+        );
+    });
+
+    it('read only one layer and return json', function () {
+        var r = folder.readDir('./test/traverse-me');
+        expect(r).to.eql(
+            [
+                {
+                    dir: true,
+                    path: 'a'
+                },
+                {
+                    dir: false,
+                    path: 'c.md'
+                }
+            ]
+        );
+    });
+});
