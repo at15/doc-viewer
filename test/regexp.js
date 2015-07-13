a7aef5da19223d34f2737de8bcd5b6e61628d2dc
at15 <at15@outlook.com>
2015-07-13 10:48:45 +0800

    Add match for folder in route
    - add regexp test for regular expressions used in route
    - match folder and list all the files in the folder

---
+++ test/regexp.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Pillar on 2015/7/13.
+ */
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+var regexp = require('../lib/util/regexp');
+
+describe('Regexp match routes', function () {
+    it('match md files', function () {
+        var mdPattern = regexp.mdFile;
+        var shouldMatch = '/foo/bar.md';
+        var shouldNotMatch = '/foo/bar.md/bar.txt';
+        expect(shouldMatch.match(mdPattern)[1]).to.equal('foo/bar.md');
+        expect(shouldNotMatch.match(mdPattern)).to.equal(null);
+    });
+
+    it('match non file routes', function () {
+        var folderPattern = regexp.folder;
+        var shouldNotMatch = '/foo/bar.css';
+        var shouldMatch = '/foo/bar'; // TODO:what if someone has a folder called bar.css
+        var shouldAlsoMatch = '/foo/bar/';
+        expect(shouldNotMatch.match(folderPattern)).to.equal(null);
+        expect(shouldMatch.match(folderPattern)[1]).to.equal('foo/bar');
+        // TODO: should improve the regexp so they all return string without the trailing slash
+        expect(shouldAlsoMatch.match(folderPattern)[1]).to.equal('foo/bar/');
+    });
+});
