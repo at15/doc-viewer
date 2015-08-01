1e3b0fd0e242c66fcc97318c2b4180cb9f69a696
at15 <at15@outlook.com>
2015-08-01 23:03:40 +1000

    Add live reload for express server
    - use fork to create a child process
    - call kill() on the child process
    
    so we can restart the child process automatically. Also the log for forked
    child process is also showing.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@
 
 'use strict';
 
+var childProcess = require('child_process');
+var serverProcess = false;
+
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var mocha = require('gulp-mocha');
@@ -15,11 +18,10 @@ var scssFiles = [
     './views/assets/style/*.scss',
     './views/assets/style/**/*.scss'
 ];
-
-//var jsFiles = [
-//    './lib/*.js',
-//    './lib/**/*.js'
-//];
+var jsFiles = [
+    './lib/*.js',
+    './lib/**/*.js'
+];
 
 gulp.task('lint', function () {
     return gulp.src([
@@ -43,13 +45,32 @@ gulp.task('build', ['lint', 'style'], function () {
     console.log('Building...');
 });
 
+gulp.task('stop-server', function () {
+    if (serverProcess) {
+        //serverProcess.send({cmd: 'SIGTERM'});
+        serverProcess.kill();
+    }
+    //serverProcess = childProcess.fork('./index');
+    //// TODO:this won't work.
+    //serverProcess.stdout.on('data', function (data) {
+    //    console.log('stdout: ' + data);
+    //});
+    //require('./index');
+});
+
+gulp.task('start-server', function () {
+    serverProcess = childProcess.fork('./index');
+    console.log(serverProcess.pid);
+});
+
 // TODO: (auto reload may also be a good thing)
 gulp.task('watch', function () {
     gulp.watch(scssFiles, ['build']).on('change', function (file) {
         console.log(file.path, ' changed');
     });
-    // TODO: auto reload
-    // gulp.watch(jsFiles, ['lint'])
+    gulp.watch(jsFiles, ['stop-server', 'start-server']).on('change', function (file) {
+        console.log(file.path, ' changed ');
+    });
 });
 
 gulp.task('test', ['lint'], function () {
@@ -59,11 +80,8 @@ gulp.task('test', ['lint'], function () {
         .pipe(mocha());
 });
 
-gulp.task('server', function () {
-    require('./index');
-});
 
-gulp.task('dev', ['build', 'server', 'watch'], function () {
+gulp.task('dev', ['build', 'watch', 'start-server'], function () {
 
 });
 
