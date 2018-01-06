80e66d766a281af0a9f04d890803f2c3bf37d2ef
at15 <at15@outlook.com>
2018-01-05 21:58:01 -0800

    Fix broken tests and set Travis to use node 8
    - it was using 0.10, 0.11, 0.12 .... time flies
    - package-lock.json is ignored

--- test/render.js
+++ test/render.js
@@ -11,26 +11,24 @@ describe('Test if gfm is working', function () {
     it('disable gfm render', function () {
         expect(render.noGfm('#haha')).to.equal('<h1 id="haha">haha</h1>\n');
     });
-    // TODO: what does gfm have?
+    // NOTE: #ha is not header in GFM, a space is needed # ha
     it('enable gfm when render', function () {
-        // NOTE: This is a gfm feature?
-        expect(render.gfm('#haha')).to.equal('<h1 id="haha">haha</h1>\n');
+        expect(render.gfm('# haha')).to.equal('<h1 id="haha">haha</h1>\n');
     });
 });
 
 describe('Test code highlight', function () {
     // TODO: highlight is really slow, 150ms....
     it('highlight js', function () {
-        expect(render.highlight('````javascript\nvar s = 1\n````')).to.equal('<pre>' +
-            '<code class="lang-javascript">' +
-            '<span class="hljs-variable"><span class="hljs-keyword">var</span> s</span> = <span class="hljs-number">1</span>\n' +
-            '</code></pre>\n');
+        expect(render.highlight('````javascript\nvar s = 1\n````')).to.equal(
+            '<pre><code class="lang-javascript"><span class="hljs-selector-tag">var</span> s = <span class="hljs-number">1</span>\n</code></pre>\n'
+        );
     });
 });
 
 describe('Test toc', function () {
     it('return rendered toc and content at same time', function () {
-        expect(render.withToc('#ha')).to.eql({
+        expect(render.withToc('# ha')).to.eql({
             toc: '<a href="#ha">ha</a>',
             content: '<h1 id="ha">ha</h1>'
         });
